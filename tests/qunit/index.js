f108b20ec08820a56a9f4b62beb4478b12e91822
Rashad Aliyev <aliyev.resad@gmail.com>
2024-05-15 18:35:00 +0400

    new action launch

--- tests/qunit/index.js
+++ tests/qunit/index.js
@@ -12,6 +12,39 @@ fs.readdirSync(specsFolder).forEach(file => {
     }
 });
 
+
+// QUnit.test("last test", function(assert) {
+//     assert.ok(true, "Passed!");
+// });
+// ModuleDoneDetails
+// QUnit.testDone( function( details ) {
+//     console.log("Finished running: ", details.name,
+//         `; \nFailed: ${details.failed} / Total: ${details.total}`
+//     );
+
+//     if (details.failed > 0) {
+//         QUnit.config.queue.length = 0;
+//         process.env.failedTests = details.failed;
+//         process.exit(1, `Test failed on: ${details.module} - ${details.name}`);
+//     }
+//     if (details.name === "last test") {
+//         QUnit.config.queue = [];
+//         process.env.failedTests = details.failed;
+//         console.log("All tests passed!");
+//         // process.exit(0);
+//     }
+// });
+
+QUnit.done((report) => {
+  const { failed, passed, runtime, total } = report;
+  process.env.QUNIT_TOTAL = total;
+  process.env.QUNIT_FAILED = failed;
+  process.env.QUNIT_PASSED = passed;
+  process.exit(failed > 0 ? 1 : 0);
+});
+
+
+// Trouble on Github Actions implementation
 // QUnit.autostart = false;
 // let promises = []
 // fs.readdir(specsFolder, (err, files) => {
@@ -51,22 +84,3 @@ fs.readdirSync(specsFolder).forEach(file => {
 //         });
 //     })
 //     .catch(err => console.log(`error: ${err}`));
-
-QUnit.test("last test", function(assert) {
-    assert.ok(true, "Passed!");
-});
-
-QUnit.testDone( function( details ) {
-    console.log("Finished running: ", details.name,
-        `; \nFailed: ${details.failed} / Total: ${details.total}`
-    );
-    console.log(`export failed=${details.failed};`);
-    if (details.failed > 0) {
-        QUnit.config.queue.length = 0;
-        process.exit(1, `Test failed on: ${details.module} - ${details.name}`);
-    }
-    if (details.name === "last test") {
-        QUnit.config.queue.length = 0;
-        process.exit(0);
-    }
-});
