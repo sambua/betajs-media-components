ffdfe0eb86cec58657dfa97770c4d22873985660
Rashad Aliyev <aliyev.resad@gmail.com>
2024-05-16 15:52:39 +0400

    updated package lock file

--- tests/qunit/index.js
+++ tests/qunit/index.js
@@ -4,25 +4,25 @@ const specsFolder = require("path").join(__dirname, "specs");
 
 QUnit.module('Player');
 
-// const includeFiles = (dir, file) => {
-//     file = file || [];
-//     const inputs = fs.readdirSync(dir);
-//     for (var i in inputs) {
-//         var name = dir + '/' + inputs[i];
-//         if (fs.statSync(name).isDirectory()){
-//             includeFiles(name, file);
-//         } else {
-//             file.push(name);
-//             if (name.match(/\.spec.js$/) === null) {
-//                 console.log(`Skipping ${name} as it's not ends with .spec.js`);
-//             } else {
-//               require(name);
-//             }
-//         }
-//     }
-//     return file;
-// }
-// if (process.env?.CI) includeFiles(specsFolder);
+const includeFiles = (dir, file) => {
+    file = file || [];
+    const inputs = fs.readdirSync(dir);
+    for (var i in inputs) {
+        var name = dir + '/' + inputs[i];
+        if (fs.statSync(name).isDirectory()){
+            includeFiles(name, file);
+        } else {
+            file.push(name);
+            if (name.match(/\.spec.js$/) === null) {
+                console.log(`Skipping ${name} as it's not ends with .spec.js`);
+            } else {
+              require(name);
+            }
+        }
+    }
+    return file;
+}
+if (process.env?.CI) includeFiles(specsFolder);
 
 QUnit.testDone((details) => {
   console.log(`Details: `, details);
