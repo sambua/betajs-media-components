2e03ab5584f59cc0a5397e384805dec147ce3bdc
Rashad Aliyev <aliyev.resad@gmail.com>
2024-05-16 16:04:21 +0400

    updated package lock file

--- tests/qunit/player.js
+++ tests/qunit/player.js
@@ -1,6 +1,8 @@
+ const { BetaJS, container } = require('./init');
+
  const runner = (attrs) => {
     const player = new BetaJS.MediaComponents.VideoPlayer.Dynamics.Player({
-        element: require('./init').container,
+        element: container,
         attrs: attrs || {}
     });
     player.activate();
